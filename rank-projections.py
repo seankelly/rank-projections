b04907d195f9a3bc3af935258949dbcf26fe4a84
Sean Kelly <code@katron.org>
2013-03-10 20:04:19 -0500

    Initial commit
    Create script to average multiple projections and use a different system
    for estimating playing time to calculate an overall player ranking.

---
+++ rank-projections.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import argparse
+import csv
+
+class Projection():
+    def __init__(self, file, *stats):
+        self.file = file
+        self.players = []
+
+    def _load_players(self):
+        pass
+
+if __name__ == '__main__':
+    pass
